e809facbaeea7a37984dd69525ea74f492651a5d
Luke Todd <luketodd@zoho.com>
2021-02-15 15:23:10 +0000

    Revert to component

--- src/VirtualGrid.vue
+++ src/VirtualGrid.vue
@@ -454,9 +454,7 @@ export default class VirtualGrid<P> extends Vue {
                     'grid-row-start': getGridRowStart(item, renderData),
                 }"
             >
-                <slot
-                    :item="item"
-                />
+                 <component :is="item.renderComponent" :item="item" v-on="$listeners" />
             </div>
         </div>
         <component :is="loadingBatch && loader" />
